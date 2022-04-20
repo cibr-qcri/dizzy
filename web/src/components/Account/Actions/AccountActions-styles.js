3dd26821c7c647e83ac094bb3f81ca631da12037
Yazan Boshmaf <yazan.boshmaf@gmail.com>
2022-04-20 09:51:10 +0300

    Issue #36: Replaced buttons with blocking buttons when busy

--- web/src/components/Account/Actions/AccountActions-styles.js
+++ web/src/components/Account/Actions/AccountActions-styles.js
@@ -1,5 +1,6 @@
 // Components
 import PromptDialogRaw, { promptDialogStyler } from '../../PromptDialogRaw';
+import ButtonRaw, { buttonStyler } from '../../ButtonRaw';
 
 // Utils
 import { makeStyles, withStyles } from '../../../utils';
@@ -9,8 +10,9 @@ export const stylesCreator = (theme) => ({
     root: {
       width: '100%',
       maxWidth: 460,
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2),
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      marginTop: theme.spacing(1),
     },
     button: {
       width: '100%',
@@ -21,6 +23,12 @@ export const stylesCreator = (theme) => ({
     },
   },
   PromptDialogRaw: promptDialogStyler(theme).Default,
+  ButtonRaw: {
+    ...buttonStyler(theme).Default,
+    root: {
+      margin: theme.spacing(1),
+    },
+  },
 });
 
 // Local
@@ -28,3 +36,4 @@ export const useStyles = makeStyles(stylesCreator);
 
 // HOCs
 export const PromptDialog = withStyles(stylesCreator, PromptDialogRaw);
+export const Button = withStyles(stylesCreator, ButtonRaw);
